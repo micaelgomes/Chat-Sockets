f37c10631dda9144f738582b7a820b802d22f5f9
micaelgoms <micaellgoms@gmail.com>
2018-11-26 22:36:35 -0300

    estilização dos componenter

--- server.js
+++ server.js
@@ -1,10 +1,35 @@
 var app = require('express')();
 var http = require('http').Server(app);
+var io = require('socket.io')(http);
 
-app.get('/', (req, res) => {
-    res.send("<h1>Server Start!</h1>");
+var clients = {}; 
+
+app.get('/', function(req, res){
+  res.send('<h1>Servidor de mensagens habilitado.</h1>');
 });
 
-http.listen(8082, () => {
-    console.log('ouvindo na Porta 8082');
+io.on("connection", function (client) {  
+    client.on("join", function(name){
+    	console.log("entrou: " + name);
+        clients[client.id] = name;
+        client.emit("update", "Você entrou na Sala de bate Papo.");
+        client.emit("update", "Nesta sala é proibido mandar Nudes");
+        client.broadcast.emit("update", name + " entrou na Sala.")
+    });
+
+    client.on("send", function(msg){
+    	console.log("Message: " + msg);
+        client.broadcast.emit("chat", clients[client.id], msg);
+    });
+
+    client.on("disconnect", function(){
+    	console.log("Disconnect");
+        io.emit("update", clients[client.id] + " deixou a sala.");
+        delete clients[client.id];
+    });
+});
+
+
+http.listen(8082, function(){
+  console.log('listening on port 8082');
 });
